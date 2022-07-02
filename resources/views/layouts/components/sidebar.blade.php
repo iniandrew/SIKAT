887e3b3634d214c488e8a6d7e085f307517a20e2
andrew <iniandrewrp@gmail.com>
2022-07-03 03:42:52 +0700

    update sidebar

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -7,36 +7,36 @@
     </div>
     <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
-        <li class="nav-item"><a href="{{ route('app.dashboard') }}" class="nav-link"><i class="fas fa-home"></i><span>Dashboard</span></a></li>
+        <li class="nav-item {{ request()->routeIs('app.dashboard') ? 'active' : '' }}"><a href="{{ route('app.dashboard') }}" class="nav-link"><i class="fas fa-home"></i><span>Dashboard</span></a></li>
 
         <li class="menu-header">Menu</li>
 
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown {{ request()->routeIs('app.agenda.*') ? 'active' : '' }}">
             <a href="#" class="nav-link has-dropdown"><i class="fas fa-calendar-alt"></i><span>Agenda</span></a>
             <ul class="dropdown-menu">
-                <li class="active"><a class="nav-link" href="{{ route('app.agenda.index') }}">List Agenda</a></li>
-                <li><a class="nav-link" href="{{ route('app.agenda.create') }}">Tambah Agenda</a></li>
+                <li class="{{ request()->routeIs('app.agenda.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.agenda.index') }}">List Agenda</a></li>
+                <li class="{{ request()->routeIs('app.agenda.create') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.agenda.create') }}">Tambah Agenda</a></li>
             </ul>
         </li>
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown {{ request()->routeIs('app.aduan.*') ? 'active' : '' }}">
             <a href="#" class="nav-link has-dropdown"><i class="fas fa-list"></i><span>Aduan</span></a>
             <ul class="dropdown-menu">
-                <li class=""><a class="nav-link" href="{{ route('app.aduan.index') }}">List Aduan</a></li>
-                <li><a class="nav-link" href="{{ route('app.aduan.create') }}">Tambah Aduan</a></li>
+                <li class="{{ request()->routeIs('app.aduan.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.aduan.index') }}">List Aduan</a></li>
+                <li class="{{ request()->routeIs('app.aduan.create') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.aduan.create') }}">Tambah Aduan</a></li>
             </ul>
         </li>
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown {{ request()->routeIs('app.dana.*') ? 'active' : '' }}">
             <a href="#" class="nav-link has-dropdown"><i class="fas fa-wallet"></i><span>Pendanaan</span></a>
             <ul class="dropdown-menu">
-                <li class=""><a class="nav-link" href="{{ route('app.dana.index') }}">List Dana</a></li>
-                <li><a class="nav-link" href="{{ route('app.dana.create') }}">Tambah Pendanaan</a></li>
+                <li class="{{ request()->routeIs('app.dana.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.dana.index') }}">List Dana</a></li>
+                <li class="{{ request()->routeIs('app.dana.create') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.dana.create') }}">Tambah Pendanaan</a></li>
             </ul>
         </li>
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown {{ request()->routeIs('app.warga.*') ? 'active' : '' }}">
             <a href="#" class="nav-link has-dropdown"><i class="fas fa-users"></i><span>Data Warga</span></a>
             <ul class="dropdown-menu">
-                <li class=""><a class="nav-link" href="{{ route('app.warga.index') }}">List Warga</a></li>
-                <li><a class="nav-link" href="{{ route('app.warga.index') }}">Tambah Warga</a></li>
+                <li class="{{ request()->routeIs('app.warga.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.warga.index') }}">List Warga</a></li>
+                <li class="{{ request()->routeIs('app.warga.create') ? 'active' : '' }}"><a class="nav-link" href="{{ route('app.warga.index') }}">Tambah Warga</a></li>
             </ul>
         </li>
         <li class="nav-item dropdown">
